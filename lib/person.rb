4ffa70b4cb407095eeaf136b47129f9efbef3b45
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-31 00:30:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,5 +4,8 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initailize
+  def initailize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
 end
